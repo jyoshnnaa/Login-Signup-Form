78a5d5b0cc911d7028bb2e265d0b7f5b53e3f71e
jyoshnnaa <jyoshna.maraboyina@gmail.com>
2025-08-15 23:25:10 +0530

    Update frontend API to deployed backend, backend fixes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, // backend URL
+  baseURL: 'https://login-signup-form-4cck.onrender.com/api', // backend URL
 });
 
 export const loginUser = (data) => API.post('/auth/login', data);
